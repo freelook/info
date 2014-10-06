c0a8713a84cf11051df1134ea26f8e78fc948d99
freelook <freelook@mail.ua>
2014-10-06 11:49:09 +0300

    add less build

--- config/env/all.js
+++ config/env/all.js
@@ -1,45 +1,43 @@
 'use strict';
 
 module.exports = {
-	app: {
-		title: 'freelook',
-		description: 'freelook.info',
-		keywords: 'MongoDB, Express, AngularJS, Node.js'
-	},
-	port: process.env.PORT || 3000,
-	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
-	sessionCollection: 'sessions',
-	assets: {
-		lib: {
-			css: [
-				'app/lib/mobile-angular-ui/dist/css/mobile-angular-ui-base.css',
-				'app/lib/mobile-angular-ui/dist/css/mobile-angular-ui-hover.css'
-			],
-			js: [
-				'app/lib/angular/angular.js',
-				'app/lib/angular-route/angular-route.js',
-				'app/lib/angular-touch/angular-touch.js',
-				'app/lib/mobile-angular-ui/dist/js/mobile-angular-ui.js',
+    app: {
+        title: 'freelook',
+        description: 'freelook.info',
+        keywords: 'MongoDB, Express, AngularJS, Node.js'
+    },
+    port: process.env.PORT || 3000,
+    templateEngine: 'swig',
+    sessionSecret: 'MEAN',
+    sessionCollection: 'sessions',
+    assets: {
+        lib: {
+            css: [],
+            js: [
+                'app/lib/angular/angular.js',
+                'app/lib/angular-route/angular-route.js',
+                'app/lib/angular-touch/angular-touch.js',
+                'app/lib/mobile-angular-ui/dist/js/mobile-angular-ui.js',
 
-				'app/lib/angular-resource/angular-resource.js',
-				'app/lib/angular-cookies/angular-cookies.js',
-				'app/lib/angular-animate/angular-animate.js',
-				'app/lib/angular-sanitize/angular-sanitize.js'
-			]
-		},
-		css: [
-			'app/modules/**/css/*.css'
-		],
-		js: [
-			'app/config.js',
-			'app/application.js',
-			'app/modules/*/*.js',
-			'app/modules/*/*[!tests]*/*.js'
-		],
-		tests: [
-			'app/lib/angular-mocks/angular-mocks.js',
-			'app/modules/*/tests/*.js'
-		]
-	}
+                'app/lib/angular-resource/angular-resource.js',
+                'app/lib/angular-cookies/angular-cookies.js',
+                'app/lib/angular-animate/angular-animate.js',
+                'app/lib/angular-sanitize/angular-sanitize.js'
+            ]
+        },
+        css: [
+            'app/modules/**/css/*.css',
+            'app/less/base.css'
+        ],
+        js: [
+            'app/config.js',
+            'app/application.js',
+            'app/modules/*/*.js',
+            'app/modules/*/*[!tests]*/*.js'
+        ],
+        tests: [
+            'app/lib/angular-mocks/angular-mocks.js',
+            'app/modules/*/tests/*.js'
+        ]
+    }
 };
