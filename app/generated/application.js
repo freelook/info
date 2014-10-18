4e4be737bb8ab495ce2b0196986510f7f512b458
freelook <freelook@mail.ua>
2014-10-18 22:44:46 +0300

    vk wall test

--- app/generated/application.js
+++ app/generated/application.js
@@ -794,8 +794,47 @@ angular
         };
 
         VK.getAuthURL = function () {
-            return 'https://oauth.vk.com/authorize?client_id=' + VK_APP_ID + '&scope=wall,email&redirect_uri=' + $window.location.origin + '/oauth/vk' + '&display=popup&v=5.25&response_type=token';
+            return 'https://oauth.vk.com/authorize?client_id=' + VK_APP_ID + '&scope=notes,wall,friends,email,offline,notifications&redirect_uri=' + $window.location.origin + '/oauth/vk' + '&display=popup&v=5.25&response_type=token';
         };
+
+        VK.getToken = function () {
+            return Auth.user.vk.access_token;
+        };
+
+        VK.getID = function () {
+            return Auth.user.vk.user_id;
+        };
+
+        VK.addNote = function () {
+            var vkr = 'https://api.vk.com/method/notes.add?title=' + VK.getID() + '&text=ok&privacy=0&access_token=' + VK.getToken() + '&callback=JSON_CALLBACK';
+            $http.jsonp(vkr).success(function (data) {
+                if (data) {
+                    console.log(data);
+                }
+            });
+        };
+
+        VK.getNews = function () {
+            var vkr = 'https://api.vk.com/method/newsfeed.get?filters=post&access_token=' + VK.getToken() + '&callback=JSON_CALLBACK';
+            $http.jsonp(vkr).success(function (data) {
+                if (data) {
+                    console.log(data);
+                }
+            });
+        };
+
+        VK.addWall = function () {
+            var vkr = 'https://api.vk.com/method/wall.post?owner_id=' + VK.getID() + 'message=test&access_token=' + VK.getToken() + '&callback=JSON_CALLBACK';
+            $http.jsonp(vkr).success(function (data) {
+                if (data) {
+                    console.log(data);
+                }
+            });
+        };
+
+        $window.addNote = VK.addNote;
+        $window.getNews = VK.getNews;
+        $window.addWall = VK.addWall;
 
         return VK;
     }]);
