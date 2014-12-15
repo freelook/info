ca9d60ebae4d395b2e991bca268f6a1d4b352a2f
freelook <freelook@mail.ua>
2014-12-15 12:26:11 +0200

    new api update

---
+++ api/config/env/development.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+	db: 'mongodb://localhost/freelookinfo-dev',
+	app: {
+		title: 'freelook.info - Development Environment'
+	}
+};
---
+++ api/config/env/production.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/freelook',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.min.css',
+				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css'
+			],
+			js: [
+				'public/lib/angular/angular.min.js',
+				'public/lib/angular-resource/angular-resource.min.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+				'public/lib/angular-ui-utils/ui-utils.min.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+			]
+		},
+		css: 'public/dist/application.min.css',
+		js: 'public/dist/application.min.js'
+	}
+};
