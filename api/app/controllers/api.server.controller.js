46033c3f7ca02b207e0c8cd5d71d88fe82eded3c
freelook <freelook@mail.ua>
2014-12-30 14:44:43 +0200

    api fix

--- api/app/controllers/api.server.controller.js
+++ api/app/controllers/api.server.controller.js
@@ -5,9 +5,9 @@ var $http = require('request'),
     regURL = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
 exports.get = function (req, res) {
-    if (req.query.url && regURL.test(req.query.url)) {
+    if (req.query && req.query.url && regURL.test(req.query.url)) {
 
-        $http.get('http://service.prerender.io/' + decodeURIComponent(req.query.url), function (err, response, body) {
+        $http.get('http://service.prerender.io/' + req.query.url, function (err, response, body) {
             if (!err && +response.statusCode === +200) {
                 res.jsonp(body);
             } else {
@@ -15,6 +15,10 @@ exports.get = function (req, res) {
                     Error: 1
                 });
             }
+        }).on('error', function (err) {
+            res.jsonp({
+                Error: err
+            });
         });
     } else {
         res.jsonp({
