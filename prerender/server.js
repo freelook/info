bca5d80c9b56c27e1f0122b0a8ce608273432a09
freelook <freelook@mail.ua>
2015-01-29 18:13:11 +0200

    update prerender

--- prerender/server.js
+++ prerender/server.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var prerender = require('./lib/index');
+var prerender = require('./lib');
 
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
@@ -11,12 +11,10 @@ var server = prerender({
 
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-//server.use(prerender.blacklist());
+server.use(prerender.blacklist());
 // server.use(prerender.logger());
-
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
