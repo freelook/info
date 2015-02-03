f7ba73aec57dd3adbbe5540a90ddcde12c06617f
freelook <freelook@mail.ua>
2015-02-03 16:55:17 +0200

    add cache for prerender

--- prerender/server.js
+++ prerender/server.js
@@ -12,10 +12,10 @@ var server = prerender({
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
-// server.use(prerender.logger());
+//server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
+//server.use(prerender.httpHeaders());
+server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
 server.start();
