10c2a1fe6111162105da7825c3863bfe591d019d
freelook <freelook@mail.ua>
2015-02-26 18:36:46 +0200

    short directive find html

--- app/src/components/look/short/short.directive.js
+++ app/src/components/look/short/short.directive.js
@@ -1,35 +1,117 @@
 'use strict';
 angular.module('fli.look')
-  .directive('fliLookShort', function ($rootScope, prerender) {
+  .directive('fliLookShort', function ($rootScope, $sce, $window, prerender) {
     return {
       scope: true,
       templateUrl: 'components/look/short/short.html',
       link: function (scope) {
 
-        scope.shortData = {};
+        var dom = '',
+          body = '';
+        scope.shortData = [];
 
-        function shortHTML(html, by) {
-          var dom = (new window.DOMParser()).parseFromString(html, 'text/html');
-          var short = [];
+        function fixLinks(html) {
+          html.find('a').each(function (i, e) {
+            $(e).attr('href', function (i, value) {
+              if (value) {
+                switch (value.substr(0, 1)) {
+                  case 'h':
+                    break;
+                  case '/':
+                    value = $rootScope.fli.route.url + value;
+                    break;
+                  case 'm':
+                    return value;
+                  default:
+                    value = $rootScope.fli.route.url + '/' + value;
+                }
+              }
+              return 'http://' + $window.location.host + '/look?url=' + value + '&input=' + $rootScope.fli.route.input + '&type=short';
+            });
+          });
+        }
 
-          $('*:contains(' + $rootScope.fli.route[by] + ')', $('body', dom))
+        function findHtml(dom, by) {
+          var div = null;
+          if (!body) {
+            body = $('body', dom);
+            body.find('script, iframe').remove();
+            fixLinks(body);
+          }
+          $('*:contains(' + by + ')', body)
             .each(function (i, e) {
 
               var element = $(e);
 
               if (!element.children().length) {
-                short.push({html: element.parent().text()});
+                if (!div) {
+                  div = $('<div>');
+                }
+                div.append(element.parent());
               }
 
             });
 
+          return div;
+        }
+
+        function shortHTML(html, by) {
+
+          var short = [],
+            _findHtml;
+          dom = !dom ? (new window.DOMParser()).parseFromString(html, 'text/html') : dom;
+          _findHtml = findHtml(dom, by);
+
+          if (_findHtml) {
+            _findHtml.each(function (i, e) {
+              var html = $(e).html();
+              if (html) {
+                short.push({html: $sce.trustAsHtml(html)});
+              }
+            });
+          }
+
           return !!short.length ? short : '';
+        }
+
+        function shortDataByContext(html, by) {
+          var data = null;
+          [by.context, by.input].some(function (e) {
+            data = shortHTML(html, e);
+            return !!data;
+          });
+          return data;
+        }
+
+        function shortDataByInput(html, by) {
+          var data = null,
+            input = by.input || '',
+            inputs = input.split(' ');
+          if (inputs.length > 2) {
+            inputs.sort(function (a, b) {
+              return b.length - a.length;
+            });
+            inputs.some(function (e) {
+              data = shortHTML(html, e);
+              return !!data;
+            });
+          }
+          return data;
         }
 
 
         if ($rootScope.fli.route.url) {
           prerender.get($rootScope.fli.route.url).then(function (html) {
-            scope.shortData = shortHTML(html, 'context') || shortHTML(html, 'input') || [];
+            try {
+              var by = $rootScope.fli.route;
+              $rootScope.fli.progress = true;
+              scope.shortData = shortDataByContext(html, by) || shortDataByInput(html, by);
+            }
+            finally {
+              $rootScope.fli.progress = false;
+              dom = null;
+            }
+
           });
         }
 
