5247a448d4b39b9f850205d6ead6a6c5354bf4ff
freelook <freelook@mail.ua>
2015-04-21 17:35:10 +0300

    add mongorestore script

--- tasks/mongodump.sh
+++ tasks/mongodump.sh
@@ -6,4 +6,4 @@ read username;
 echo "password:";
 read password;
 
-mongodump --host 185.65.244.51:27017 --username $username --password $password
+mongodump --host 185.65.244.51:27017 --db test --username $username --password $password --authenticationDatabase admin
---
+++ tasks/mongorestore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "username:";
+read username;
+
+echo "password:";
+read password;
+
+mongorestore --host 185.65.244.51:27017 --db test --username $username --password $password --authenticationDatabase admin ./dump/test
