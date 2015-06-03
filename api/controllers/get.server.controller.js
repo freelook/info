a7d75924985e6a2864016998b7bcfb2fa70f77a6
freelook <freelook@mail.ua>
2015-06-03 18:26:33 +0300

    fix api get res on close (cherry picked from commit 6a6727c) (cherry picked from commit 6098180)

--- api/controllers/get.server.controller.js
+++ api/controllers/get.server.controller.js
@@ -4,24 +4,30 @@ var $http = require('request');
 
 module.exports = function (req, res) {
     if (req.query && req.query.url) {
-        $http
+        var _req = $http
             .get({
                 url: encodeURI(decodeURI(req.query.url)),
                 headers: {
                     'User-Agent': 'googlebot'
                 }
             })
-            .pipe(res)
             .on('error', function (err) {
                 res.status(404).json({
                     err: err || true,
                     msg: 'error request'
                 });
-            });
+            })
+            .pipe(res);
     } else {
         res.status(404).json({
             err: true,
             msg: 'error url'
         });
     }
+
+    res.on('close', function () {
+        if (_req && typeof _req.destroy === 'function') {
+            _req.destroy();
+        }
+    });
 };
