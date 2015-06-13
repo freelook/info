4fbf6c5299a3cde5b18f111ab6d26ddef12f73bb
freelook <freelook@mail.ua>
2015-06-13 14:56:58 +0300

    add vk search + play music

---
+++ app/src/components/search/result/web/vk/vk.controller.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular
+  .module('fli.search')
+  .controller('search.result.web.vk.ctrl',
+  function ($scope, $sce, CONFIG, vk) {
+
+    var vm = this;
+    vm.search = {};
+
+    function _getUrl(id) {
+      return CONFIG.ORIGIN + 'look?input=' + $scope.fli.route.input + '&url=' + _getLink(id);
+    }
+
+    function _getLink(id) {
+      return 'https://vk.com/' + id;
+    }
+
+    vm.link = function (id) {
+      return _getLink(id);
+    };
+
+    vm.href = function (id) {
+      return _getUrl(id);
+    };
+
+    vm.audio = function (_url) {
+      return $sce.trustAsResourceUrl(_url);
+    };
+
+    function setResult(vk) {
+      var result = vk.response || [];
+      vm.results = angular.copy(result).splice(1);
+      console.log(vm.results);
+    }
+
+    if ($scope.fli.route.input) {
+      vk.search($scope.fli.route.input)
+        .success(setResult);
+    }
+
+  });
+
