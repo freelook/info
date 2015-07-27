2d1026ceef82bd52de268b2b44aeb4ad53b55965
freelook <freelook@mail.ua>
2015-07-27 18:43:21 +0300

    add link to home

--- app/src/components/_index/footer/footer.controller.js
+++ app/src/components/_index/footer/footer.controller.js
@@ -2,11 +2,11 @@
 
 angular
   .module('freelook.info')
-  .controller('footer.ctrl', function (url) {
+  .controller('footer.ctrl', function ($location, url) {
 
     var vm = this;
     vm.donate = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RFP3HDDXV2NJS';
-    vm.company = url.href('company');
+    vm.company = $location.path() !== '/company' ? url.href('company') : url.href('');
 
   });
 
