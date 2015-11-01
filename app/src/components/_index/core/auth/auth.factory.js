9b61c47d61b4186871b0ecfcd4ad5fae3e4d30bd
freelook <freelook@mail.ua>
2015-11-01 14:01:56 +0200

    add site auth

--- app/src/components/_index/core/auth/auth.factory.js
+++ app/src/components/_index/core/auth/auth.factory.js
@@ -1,32 +1,14 @@
 'use strict';
 angular
   .module('freelook.info')
-  .factory('auth', function ($rootScope, $timeout, user, Parse) {
+  .factory('auth', function (platform, authSite) {
 
-    function _setUser() {
-      $timeout(function () {
-        $rootScope.fli.user = user.current();
-      });
-    }
-
-    function logIn() {
-      if (!user.current()) {
-        Parse.FacebookUtils.logIn(null, {
-          success: function () {
-            _setUser();
-          }
-        });
-      }
-    }
-
-    function logOut() {
-      Parse.User.logOut().then(_setUser);
-    }
-
-    return {
-      logIn: logIn,
-      logOut: logOut
+    var authNoop = {
+      logIn: angular.noop,
+      logOut: angular.noop
     };
+
+    return platform.name() === 'site' ? authSite : authNoop;
 
   });
 
---
+++ app/src/components/_index/core/auth/site.auth.factory.js
@@ -0,0 +1,33 @@
+'use strict';
+angular
+  .module('freelook.info')
+  .factory('authSite', function ($rootScope, $timeout, user, Parse) {
+
+    function _setUser() {
+      $timeout(function () {
+        $rootScope.fli.user = user.current();
+      });
+    }
+
+    function logIn() {
+      if (!user.current()) {
+        Parse.FacebookUtils.logIn(null, {
+          success: function () {
+            _setUser();
+          }
+        });
+      }
+    }
+
+    function logOut() {
+      Parse.User.logOut().then(_setUser);
+    }
+
+    return {
+      logIn: logIn,
+      logOut: logOut
+    };
+
+  });
+
+
