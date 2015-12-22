73ffa385e94da6bb3aa0725f1a01d4ff5b8bb22b
freelook <freelook@mail.ua>
2015-12-22 15:51:43 +0200

    pinterest search

--- app/src/components/_index/core/api/pinterest/pinterest.factory.js
+++ app/src/components/_index/core/api/pinterest/pinterest.factory.js
@@ -1,7 +1,7 @@
 'use strict';
 angular
   .module('freelook.info')
-  .factory('pinterest', function ($http, $window, $timeout, api, PINTEREST_API) {
+  .factory('pinterest', function ($http, $q, api, parser, PINTEREST_API) {
 
     function _enpoint(_end) {
       return PINTEREST_API.ENDPOINT + _end;
@@ -9,6 +9,37 @@ angular
 
     function _point(_end) {
       return $http.get(_enpoint(_end));
+    }
+
+    function _htmlToJs(_html) {
+      var js = [],
+        dom = parser.parseFromString(_html, 'text/html'),
+        $dom = $(dom);
+
+      $dom.find('.GridItems .item').each(function (i, e) {
+        var element = $(e);
+        js.push({
+          title: element.find('.pinCredits > .creditTitle').text(),
+          board: element.find('.creditItem > a').attr('href'),
+          img: element.find('img.pinImg').attr('src'),
+          content: element.find('.pinMeta > .pinDescription').text(),
+          url: element.find('.pinImageActionButtonWrapper > a').attr('href')
+        });
+      });
+
+      return js;
+    }
+
+    function search(q) {
+      var defer = $q.defer();
+      api.proxy(PINTEREST_API.URL + '/search/pins/?q=' + q)
+        .success(function (_html) {
+          defer.resolve(_htmlToJs(_html));
+        })
+        .error(function (err) {
+          defer.reject(err);
+        });
+      return defer.promise;
     }
 
     function pins(q) {
@@ -20,17 +51,20 @@ angular
     }
 
     function link(_path) {
-      return 'https://www.pinterest.com' + _path;
+      return PINTEREST_API.URL + _path;
     }
 
     return {
+      search: search,
       pins: pins,
       boards: boards,
       link: link
     };
 
   })
-  .constant('PINTEREST_API', {
-    ENDPOINT: 'https://api.pinterest.com/v3/'
+  .
+  constant('PINTEREST_API', {
+    ENDPOINT: 'https://api.pinterest.com/v3/',
+    URL: 'https://www.pinterest.com'
   });
 
