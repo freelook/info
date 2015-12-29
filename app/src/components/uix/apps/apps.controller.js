e9c6046db0e17b53c2ea7ecb2b7137369dcf7a64
freelook <freelook@mail.ua>
2015-12-29 16:18:02 +0200

    fix chrome link

--- app/src/components/uix/apps/apps.controller.js
+++ app/src/components/uix/apps/apps.controller.js
@@ -6,7 +6,7 @@ angular
   function () {
 
     var vm = this;
-    vm.chrome = 'https://chrome.google.com/webstore/detail/jlpjaecnenjbpkbcpnocbeibjokkbnhj';
+    vm.chrome = 'https://chrome.google.com/webstore/detail/freelookinfo/jlpjaecnenjbpkbcpnocbeibjokkbnhj';
     vm.android = 'https://play.google.com/store/apps/details?id=info.freelook';
 
   });
