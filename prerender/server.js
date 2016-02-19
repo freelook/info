2ab1c7ac0a8812a96bc2f89d4248e143966710fc
freelook <freelook@mail.ua>
2016-02-19 16:18:54 +0200

    update prerender

--- prerender/server.js
+++ prerender/server.js
@@ -2,22 +2,19 @@
 var prerender = require('./lib');
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    workers: process.env.PRERENDER_NUM_WORKERS,
+    iterations: process.env.PRERENDER_NUM_ITERATIONS
 });
 
 
+//server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
-// server.use(prerender.blacklist());
+//server.use(prerender.blacklist());
 // server.use(prerender.logger());
-   server.use(prerender.removeScriptTags());
-// server.use(prerender.httpHeaders());
+server.use(prerender.removeScriptTags());
+//server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
-
-require('child_process').spawn('killall', ['phantomjs']);
 
 server.start();
