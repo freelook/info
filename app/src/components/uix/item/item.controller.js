602de2d6f44849ca0df945e9cfc29ea24e2c2dea
freelook <freelook@mail.ua>
2016-03-31 17:54:46 +0300

    add url for item

--- app/src/components/uix/item/item.controller.js
+++ app/src/components/uix/item/item.controller.js
@@ -3,10 +3,14 @@
 angular
   .module('freelook.info')
   .controller('item.ctrl',
-  function (index, item) {
+  function (index, item, content) {
 
     var vm = this;
     vm.fix = index.fix;
     vm.star = item.star;
+    vm.url = function (_url) {
+      return content.site(_url).host;
+    };
+
 
   });
