87f43ec8a08fa47fe943e97b60acb868a2bc1de8
freelook <freelook@mail.ua>
2016-04-04 19:05:11 +0300

    add facebook comments

--- app/src/components/feedback/comments/comments.controller.js
+++ app/src/components/feedback/comments/comments.controller.js
@@ -3,11 +3,16 @@
 angular
   .module('fli.feedback')
   .controller('feedback.comments.ctrl',
-  function () {
+  function (locale, url, FB, CONFIG) {
 
     var vm = this;
     vm.mail = 'mail@freelook.info';
     vm.href = 'mailto:mail@freelook.info?subject=[FLI:feedback]';
+
+    vm.fbCount = 5;
+    vm.fbHref = url.href('feedback?', {l: locale.getLng()}, false, CONFIG.PRODUCTION);
+
+    FB.xfbml();
 
   });
 
