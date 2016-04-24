d220d4a367b13b241b08ddc1db16efd20257ecee
freelook <freelook@mail.ua>
2016-04-24 15:14:46 +0300

    dist: 1.14.5

--- app/src/components/core/config/config.development.js
+++ app/src/components/core/config/config.development.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   PRODUCTION: 'http://freelook.info/',
-  VERSION: '1.14.5',
+  VERSION: '1.14.6',
   API: {
     SOCKET: 'http://local.freelook.info:4000',
     URL: 'http://local.freelook.info/api/',
--- app/src/components/core/config/config.production.js
+++ app/src/components/core/config/config.production.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   PRODUCTION: 'http://freelook.info/',
-  VERSION: '1.14.4',
+  VERSION: '1.14.5',
   API: {
     SOCKET: 'http://freelook.info:4000',
     URL: 'http://freelook.info/api/',
