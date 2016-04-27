3bfe66ea15f86aed2ac2f9ce7c0a8700e2d5159b
freelook <freelook@mail.ua>
2016-04-27 23:59:03 +0300

    add amazon promo style

--- app/src/components/promo/amazon/amazon.controller.js
+++ app/src/components/promo/amazon/amazon.controller.js
@@ -3,9 +3,19 @@
 angular
   .module('fli.promo')
   .controller('promo.amazon.ctrl',
-  function ($routeParams, CONFIG) {
+  function ($window, $routeParams, $timeout, CONFIG) {
 
-    var vm = this;
+    var vm = this, eventName = 'message', bubble = false;
+
     vm.url = [CONFIG.SITE.ORIGIN, 'vendors/amazon/amazon.html?query=', $routeParams.input || ''].join('');
+
+    $window.addEventListener(eventName, function receiveMessage(msg) {
+      $window.removeEventListener(eventName, receiveMessage, bubble);
+      if (msg && msg.data === 'amazon') {
+        $timeout(function () {
+          vm.ready = true;
+        });
+      }
+    }, bubble);
 
   });
