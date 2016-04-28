8874a26c052017df343b929a3d1cbf6b7a05006f
freelook <freelook@mail.ua>
2016-04-28 16:48:29 +0300

    add amazon promo style fix

--- app/src/components/promo/amazon/amazon.controller.js
+++ app/src/components/promo/amazon/amazon.controller.js
@@ -3,19 +3,9 @@
 angular
   .module('fli.promo')
   .controller('promo.amazon.ctrl',
-  function ($window, $routeParams, $timeout, CONFIG) {
+  function ($routeParams, CONFIG) {
 
-    var vm = this, eventName = 'message', bubble = false;
-
+    var vm = this;
     vm.url = [CONFIG.SITE.ORIGIN, 'vendors/amazon/amazon.html?query=', $routeParams.input || ''].join('');
-
-    $window.addEventListener(eventName, function receiveMessage(msg) {
-      $window.removeEventListener(eventName, receiveMessage, bubble);
-      if (msg && msg.data === 'amazon') {
-        $timeout(function () {
-          vm.ready = true;
-        });
-      }
-    }, bubble);
 
   });
--- app/src/components/promo/amazon/amazon.directive.js
+++ app/src/components/promo/amazon/amazon.directive.js
@@ -2,10 +2,21 @@
 
 angular
   .module('fli.promo')
-  .directive('fliPromoAmazon', function () {
+  .directive('fliPromoAmazon', function ($window, $timeout) {
     return {
       controller: 'promo.amazon.ctrl',
       controllerAs: 'amazonCtrl',
-      templateUrl: 'components/promo/amazon/amazon.html'
+      templateUrl: 'components/promo/amazon/amazon.html',
+      link: function (scope, element) {
+        var eventName = 'message', bubble = false;
+        $window.addEventListener(eventName, function receiveMessage(msg) {
+          $window.removeEventListener(eventName, receiveMessage, bubble);
+          if (msg && msg.data === 'amazon') {
+            $timeout(function () {
+              $(element).show();
+            });
+          }
+        }, bubble);
+      }
     };
   });
