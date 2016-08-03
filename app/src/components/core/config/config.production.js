805f59b8dca4dd1975d3887def2b7ab50f1f75f7
freelook <freelook@mail.ua>
2016-08-03 09:59:51 -0700

    pack: 1.17.2

--- app/src/components/core/config/config.production.js
+++ app/src/components/core/config/config.production.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   PRODUCTION: 'http://freelook.info/',
-  VERSION: '1.17.1',
+  VERSION: '1.17.2',
   API: {
     SOCKET: 'http://freelook.info:4000',
     URL: 'http://freelook.info/api/',
